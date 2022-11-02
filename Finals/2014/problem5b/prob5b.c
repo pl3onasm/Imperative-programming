0eb42f720b327c3550f64701c0a364bbb9404ccd
David <pl3onasm@gmail.com>
2022-11-02 18:04:55 +0100

    :tada:

---
+++ Finals/2014/problem5b/prob5b.c
@@ -0,0 +1,44 @@
+/* file: prob4b.c
+   author: David De Potter
+   description: IP Final 2014, problem 5b, 
+   Recursive algorithms, kth smallest element
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int i, int j, int *a){
+  int temp = a[i];
+  a[i] = a[j];
+  a[j] = temp;
+}
+
+int partition(int *arr, int len) {
+  int idx=0, pivot = arr[0];
+  swap(0, len-1, arr);
+  for (int i = 0; i < len-1; i++)
+    if (arr[i] < pivot){
+      swap(i, idx, arr);
+      idx++;
+    }
+  swap(idx, len-1, arr);
+  return idx;
+}
+
+int kthSmallest(int *arr, int len, int k) {
+  if (len == 1) 
+    return arr[0];
+  int p = partition(arr, len);
+  if (p == k) return arr[p];
+  if (p < k) return kthSmallest(arr+p+1, len-p-1, k-p-1);
+  return kthSmallest(arr, p, k);
+}
+
+int main(int argc, char *argv[]) {
+  int n, k, arr[100], len=0;
+  scanf("%d", &k);
+  while (scanf("%d", &n) == 1) 
+    arr[len++] = n;
+  printf("%d\n", kthSmallest(arr, len, k));
+  return 0;
+}
