8f4bec0e3b8e247b36db249b6de887fb2429f4c3
David <pl3onasm@gmail.com>
2022-11-09 22:40:27 +0100

    :tada:

--- Finals/2018resit/problem5/prob5-2.c
+++ Finals/2018resit/problem5/prob5-2.c
@@ -15,20 +15,20 @@ int isPalindrome(char *s, int start, int end) {
   return isPalindrome(s, start+1, end-1);
 }
 
-int countPartitions(int idx, char *a, int size) {
+int countPartitions(int start, int end, char *a) {
   int x, y = 10000;
-  if (idx > size) return 0;
-  for (int j = idx; j <= size; j++) {
-    if (isPalindrome(a, idx, j)) {
-      x = countPartitions(j+1, a, size);
+  if (start > end) return 0;
+  for (int j = start; j <= end; j++) {
+    if (isPalindrome(a, start, j)) {
+      x = countPartitions(j+1, end, a);
       y = x < y ? x : y;
     }
   }
   return 1 + y;
 }
 
-int minimalPalPartition(int i, int size, char *a) {
-  return countPartitions (i, a, size); 
+int minimalPalPartition(int start, int end, char *a) {
+  return countPartitions (start, end, a); 
 }
 
 int main(int argc, char *argv[]) {
