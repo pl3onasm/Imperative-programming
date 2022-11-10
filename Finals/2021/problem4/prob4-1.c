10676dc82ea51eb761a96881224069c4ad89f5d8
David <pl3onasm@gmail.com>
2022-11-10 19:40:39 +0100

    :tada:

--- Finals/2021/problem4/prob4-1.c
+++ Finals/2021/problem4/prob4-1.c
@@ -1,19 +1,20 @@
-/* file: prob4.c
+/* file: prob4-1.c
    author: David De Potter
-   version: 4.1
+   version: 4.1, using a void function, computeSum, 
+      and an int pointer to keep the maxixum sum
    description: IP Final 2021, problem 4, maximum sum
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void computeSum(int arr[], int n, int index, int sum, int len, int *max) {
-  if (index >= n) {
+void computeSum(int arr[], int idx, int sum, int len, int *max) {
+  if (idx < 0) {
     if (len > 1 && sum > *max) *max = sum;
     return; 
   } 
-  computeSum(arr, n, index + 1, sum, len, max); // skip
-  computeSum(arr, n, index + 2, sum + arr[index], len+1, max); // take
+  computeSum(arr, idx-1, sum, len, max);              // skip
+  computeSum(arr, idx-2, sum + arr[idx], len+1, max); // take
 }
 
 int main(int argc, char **argv){
@@ -21,7 +22,7 @@ int main(int argc, char **argv){
   scanf("%d", &n);
   for(int i = 0; i < n; i++)
     scanf("%d ", &arr[i]);
-  computeSum(arr, n, 0, 0, 0, &max);
+  computeSum(arr, n-1, 0, 0, &max);
   printf("%d\n", max);
   return 0; 
 }
