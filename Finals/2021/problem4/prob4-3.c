8a4eb0d9c43b09268d268b0e52062badc8ae8175
David <pl3onasm@gmail.com>
2022-11-10 20:38:29 +0100

    :tada:

--- Finals/2021/problem4/prob4-3.c
+++ Finals/2021/problem4/prob4-3.c
@@ -1,18 +1,22 @@
 /* file: prob4-2.c
    author: David De Potter
-   version: 4.2, using an int function, getMaxSum
+   version: 4.3, same as 4.2, but using an explicit 
+      function to get maximum of two integers
    description: IP Final 2021, problem 4, maximum sum
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+int max(int a, int b) {
+  return a > b ? a : b;
+}
+
 int getMaxSum(int arr[], int idx, int sum, int len) {
   // get maximum sum of subsequences with len >= 2
-  if (idx < 0) return len >= 2 ? sum : 0; 
-  int x = getMaxSum(arr, idx - 2, sum + arr[idx], len+1); // take
-  int y = getMaxSum(arr, idx - 1, sum, len);              // skip
-  return x > y ? x : y;
+  if (idx < 0) return len >= 2 ? sum : 0;
+  return max(getMaxSum(arr, idx - 2, sum + arr[idx], len+1),
+             getMaxSum(arr, idx - 1, sum, len));
 }
 
 int main(int argc, char **argv){
@@ -23,5 +27,3 @@ int main(int argc, char **argv){
   printf("%d\n", getMaxSum(arr, n-1, 0, 0));
   return 0; 
 }
-
- 
