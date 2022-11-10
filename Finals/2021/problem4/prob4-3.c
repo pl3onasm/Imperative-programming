7bf79a965ae601981ae59b1ca0cd78672f1cd2be
David <pl3onasm@gmail.com>
2022-11-10 20:53:03 +0100

    :tada:

--- Finals/2021/problem4/prob4-3.c
+++ Finals/2021/problem4/prob4-3.c
@@ -1,4 +1,4 @@
-/* file: prob4-2.c
+/* file: prob4-3.c
    author: David De Potter
    version: 4.3, same as 4.2, but using an explicit 
       function to get maximum of two integers
@@ -15,8 +15,8 @@ int max(int a, int b) {
 int getMaxSum(int arr[], int idx, int sum, int len) {
   // get maximum sum of subsequences with len >= 2
   if (idx < 0) return len >= 2 ? sum : 0;
-  return max(getMaxSum(arr, idx - 2, sum + arr[idx], len+1),
-             getMaxSum(arr, idx - 1, sum, len));
+  return max(getMaxSum(arr, idx-2, sum + arr[idx], len+1),
+             getMaxSum(arr, idx-1, sum, len));
 }
 
 int main(int argc, char **argv){
