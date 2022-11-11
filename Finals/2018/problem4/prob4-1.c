8edfe5f6cd88b7168b1c6d2cc2afdf55c50b2522
David <pl3onasm@gmail.com>
2022-11-11 17:34:00 +0100

    :tada:

--- Finals/2018/problem4/prob4-1.c
+++ Finals/2018/problem4/prob4-1.c
@@ -1,5 +1,7 @@
-/* file: prob4.c
+/* file: prob4-1.c
    author: David De Potter
+   version: 4.1, the naive split is in O(n³),
+      the other split is in O(n²)
    description: IP Final 2018, problem 4, split3
 */
 
@@ -25,12 +27,12 @@ int maximum(int a, int b, int c){
 void naiveSplit(int *arr, int size, int *min){
   /* naive way to compute the separate sums, in which
      the same sums are computed over and over again */
-  int a=0, b, c; 
-  for (int i=0; i<size-2; ++i){
-    a += arr[i]; b=0; 
-    for (int j=i+1; j<size-1; ++j){
-      b += arr[j]; c=0;
-      for (int k=j+1; k<size; ++k){
+  int a= 0, b, c; 
+  for (int i = 0; i < size-2; ++i){
+    a += arr[i]; b = 0; 
+    for (int j = i+1; j < size-1; ++j){
+      b += arr[j]; c = 0;
+      for (int k = j+1; k < size; ++k){
         c += arr[k];
       }
       int max = maximum(a, b, c);
@@ -43,10 +45,10 @@ void split(int *arr, int size, int *min){
   /* precomputes sums over the subarrays in order
      to avoid continually recomputing partial sums */ 
   int a, b, c;
-  for (int i=1; i<size; ++i) arr[i] += arr[i-1];
+  for (int i = 1; i < size; ++i) arr[i] += arr[i-1];
   int total = arr[size-1];
-  for (int i=0; i<size-2; ++i){ 
-    for (int j=i+1; j<size-1; ++j){
+  for (int i = 0; i < size-2; ++i){ 
+    for (int j = i+1; j < size-1; ++j){
       a = arr[i]; b = arr[j] - arr[i]; 
       c = total - arr[j];
       int max = maximum(a, b, c);
