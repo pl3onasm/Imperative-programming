f67bdb3ade663e8dd6d27cdfd35b73c8b5d6badd
David <pl3onasm@gmail.com>
2022-11-11 13:22:41 +0100

    :tada:

--- Finals/2017resit/problem5/prob5-2.c
+++ Finals/2017resit/problem5/prob5-2.c
@@ -10,16 +10,14 @@
 #include <stdlib.h>
 
 int countExps(int target, int digit, int prev, int current, int product){
-  int result = 0;
-  if (digit == 9) return current + 9*product == target; 
+  if (digit >= 9) return current + 9*product == target; 
   if (prev) { // previous operator was *
-    result = countExps (target, digit+1, 0, current + product * digit, 1) 
+    return countExps (target, digit+1, 0, current + product * digit, 1) 
            + countExps (target, digit+1, 1, current, product * digit);    
   } else {    // previous operator was +
-    result = countExps (target, digit+1, 0, current + digit, 1) 
+    return countExps (target, digit+1, 0, current + digit, 1) 
            + countExps (target, digit+1, 1, current, digit);    
   }
-  return result;
 }
 
 int prodSum(int n){
