bc87a9af0717b30334a1459d3e9f9acc72e34f1a
David <pl3onasm@gmail.com>
2022-11-11 13:35:47 +0100

    :tada:

--- Finals/2017resit/problem5/prob5-2.c
+++ Finals/2017resit/problem5/prob5-2.c
@@ -1,7 +1,7 @@
 /* file: prob5-2.c
    author: David De Potter
-   version: 5.2, using an int function to compute
-      result during recursive calls
+   version: 5.2, using an int function, countExps, 
+      to compute result during recursive calls themselves
    description: IP Final 2017 Resit, problem 5, 
       sums and products
 */
@@ -10,7 +10,7 @@
 #include <stdlib.h>
 
 int countExps(int target, int digit, int prev, int current, int product){
-  if (digit >= 9) return current + 9*product == target; 
+  if (digit >= 9) return current + 9 * product == target; 
   if (prev) {   // previous operator was *
     return countExps (target, digit+1, 0, current + product * digit, 1) 
          + countExps (target, digit+1, 1, current, product * digit);    
