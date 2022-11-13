15b4af1920af737a259d88c3dd69f2130b9548ac
David <pl3onasm@gmail.com>
2022-11-13 19:29:38 +0100

    :tada:

--- Finals/2018/problem4/prob4-2.c
+++ Finals/2018/problem4/prob4-2.c
@@ -1,8 +1,7 @@
 /* file: prob4-2.c
    author: David De Potter
    version: 4.2, in this version we optimize
-      the second split from 4.1, which was in O(n²),
-      to be in O(n log n) using binary search
+      the program to be in O(n log n) using binary search
    description: IP Final 2018, problem 4, split3
 */
 
@@ -18,37 +17,42 @@ void *safeMalloc (int n) {
   return ptr;
 }
 
-int solutionExists (int *arr, int totalSum, int n, int mid){
+int solutionExists (int *arr, int n, int mid){
   /* checks if a solution can be found below mid, i.e. if  
      there exists a 3-way split such that maxsum < mid */
-  int i = 0;
-  while (arr[i++] < mid);
-  while (arr[i++] < 2*mid);
-  return totalSum - arr[i-1] < mid;
+  int i = 0, sum = 0, split = 1; 
+  while (i < n && split <= 3){
+    if (arr[i] > mid) return 0;
+    sum += arr[i];
+    if (sum > mid){
+      sum = arr[i];
+      split++;
+    }
+    i++;
+  }
+  return split == 3;
 }
 
-
-int binSearch (int *arr, int n, int right, int left) {
+int binSearch (int *arr, int n, int left, int right) {
   /* Searches through the solution space, consisting  
      of a (virtual) ordered array of increasing maxsums. 
      Returns the lower bound of the solution space */
-  while (left > right) {
-    int mid = left + (right-left)/2;
-    if (solutionExists(arr, left, n, mid)) right = mid + 1;
-    else left = mid;
+  if (left > right) return left;
+  int mid = left + (right - left) / 2;
+  if (solutionExists(arr, n, mid)) {
+    return binSearch(arr, n, left, mid-1);
   }
-  return left;
+  return binSearch(arr, n, mid+1, right);
 }
-
+    
 int main(int argc, char **argv){
-  int n;
+  int n, totalSum = 0;
   scanf("%d", &n);
   int *arr = safeMalloc(n*sizeof(int));
   for (int i=0; i < n; i++) {
     scanf("%d", &arr[i]);
-    arr[i] += arr[i-1];   // precomputes partial sums
+    totalSum += arr[i];
   }
-  int totalSum = arr[n-1];
   printf("%d\n", binSearch(arr, n, 0, totalSum)); 
   free(arr);
   return 0; 
