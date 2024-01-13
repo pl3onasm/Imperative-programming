71dce56513fd50e02cb56f48d463d3a39a3a2771
David <pl3onasm@gmail.com>
2024-01-13 14:54:02 +0100

    :tada:

--- IP-Finals/2018resit/problem5/prob5-2.c
+++ IP-Finals/2018resit/problem5/prob5-2.c
@@ -1,6 +1,6 @@
 /* file: prob5-2.c
    author: David De Potter
-   version: 5.2, using an int function, countParts
+   version: 5.2, using an int function
    description: IP Final 2018 Resit, problem 5,
    minimal palindromic partition
 */ 
@@ -8,32 +8,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 int isPalindrome(char *s, int start, int end) {
   if (start >= end) return 1;
   if (s[start] != s[end]) return 0;
-  return isPalindrome(s, start+1, end-1);
+  return isPalindrome(s, start + 1, end - 1);
 }
 
-int countParts(int start, int end, char *a) {
-  int x, y = 10000;
+int minimalPalPartition(int start, int end, char *a) {
+  int x, y = INT_MAX;
   if (start > end) return 0;
   for (int j = start; j <= end; j++) {
     if (isPalindrome(a, start, j)) {
-      x = countParts(j+1, end, a);
+      x = minimalPalPartition(j + 1, end, a);
       y = x < y ? x : y;
     }
   }
   return 1 + y;
 }
 
-int minimalPalPartition(int start, int end, char *a) {
-  return countParts (start, end, a); 
-}
-
 int main(int argc, char *argv[]) {
   char a[21];
   (void)! scanf("%s", a);
-  printf("%d\n", minimalPalPartition(0, strlen(a)-1, a));
+  printf("%d\n", minimalPalPartition(0, strlen(a) - 1, a));
   return 0;
 }
+ 
