e9c232187a935c9dda98d1d86c91c28c395b8bbe
David <pl3onasm@gmail.com>
2024-01-14 15:56:05 +0100

    :tada:

--- PF1-3/resit1-2023/problem4/prob4.c
+++ PF1-3/resit1-2023/problem4/prob4.c
@@ -7,17 +7,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-void resetCounts (char *chars) {
-  /* resets the histogram of characters */
-  for (int i = 0; i < 26; i++) chars[i] = 0;
-}
-
 int main(int argc, char *argv[]) {
-  int wLen = 0, found = 0;
-  char ch, word[200], chars[26] = {0};
+  int found = 0;
+  char ch, word[200];
   
   // read each word and check for isogram
   while (scanf("%s", word) == 1) {
+    int chars[26] = {0};    // histogram for characters in word
+    int wLen = 0;
     for (int i = 0; i < strlen(word); i++) {
       ch = word[i];
       if (ch == '.') {
@@ -33,12 +30,9 @@ int main(int argc, char *argv[]) {
       word[wLen++] = ch;
     } 
     if (wLen) {
-      printf(found ? " " : "");
-      printf("%s", word);
+      printf(found ? " %s" : "%s", word);
       found = 1;
     }
-    resetCounts(chars);
-    wLen = 0;
   }
   printf("\n");
   return 0;
